93d3fa0b3dabd4ddcb2f668e607e7e45e3507e67
Aaron Craig <aaron@nzrs.net.nz>
2015-07-03 10:38:17 +1200

    add clear function; fix bug where available_dbs wasn't updating correctly after loading new dbs

--- geoloc.py
+++ geoloc.py
@@ -1,56 +1,21 @@
 __author__ = 'craig'
 
-'''
-This module provides a unified geolocation lookup service across multiple
-databases. Useful for cross-referencing geolocation answers.
-
-Here's a quick example of how you can use it:
-
->>> import geoloc
->>> geoloc.quickload()
->>> geoloc.country_code('144.26.68.0', 'geoip')
-'US'
->>> geoloc.country_code_all('144.26.68.0')
-{'geoip': 'US', 'known_networks': None, 'dbip': None, 'ipligence': None, 'ip2location': 'US'}
-
-if you want to check for anomalies you can set a particular database as
-being an oracle. It is considered an authoritative source, so if they give
-some answer to a query it's considered the correct one.
-
->>> geoloc.country_code_all('103.26.68.0')
-{'geoip': 'AU', 'known_networks': u'NZ', 'dbip': 'AU', 'ipligence': None, 'ip2location': 'AU'}
->>> geoloc.anomalous('103.26.68.0')
-True
->>> geoloc.oracle('known_networks')
->>> geoloc.anomalous('103.26.68.0')
-False
-'''
-
-## Imports.
-## ----------------------------------------------------------------------
 
 # geolocation modules
 import GeoIP
 import IP2Location
 
 # used internally
-from radix import Radix as _Radix
 from netaddr import IPAddress as _IPAddress
 from bisect import bisect as _bisect
-import json as _json
 from collections import defaultdict as _defaultdict
 
 
 
 
-## Geoloc
-## ----------------------------------------------------------------------
-
-# dict of database name to data structure
 _dbs = {
     'geoip' : None,
     'ip2location' : None,
-    'known_networks' : None,
     'dbip' : None,
     'ipligence' : None
 }
@@ -61,72 +26,19 @@ supported_queries = ['country_code']
 # databases which have currently been loaded and are ready to be queried.
 available_dbs = []
 
-# if the oracle has an answer to a query, it's always considered the
-# correct answer and as such is privileged over other answers when it
-# comes to checking anomalies.
-oracle = None
-
-
-
-
-## Geoloc public interface.
-## ----------------------------------------------------------------------
-
-def clear():
-    global _dbs
-    global available_dbs
-    _dbs = { k : None for k in _dbs.keys() }
-    available_dbs = []
-
-def anomalous(ip_addr):
-    '''
-    Check if the given ip address is a potential geolocation anomaly
-    (if multiple dbs disagree about its country).
-
-    An address is a potential anomaly if one of the following is true:
-        - only one geolocation database could locate it
-        - multiple geolocation databases gave different answers about where it is
-
-    If you've set an oracle, an address is not considered anomalous if that
-    oracle is able to place it somewhere.
-
-    :param ip_addr: str, the ip address to check
-    :return: boolean
-    '''
-    results = country_code_all(ip_addr, filter_nones=True)
-    if oracle in results: return False # authoritative source
-    return len(results) == 1 or len(set(results.values())) != 1
-
-def find_anomalies(addresses):
-    '''
-    Find those IPs for which different geolocation services give different
-    answers about the country of origin.
-    :param addresses: [str] an iterable of ip addresses
-    :return: radix tree of strings.
-    '''
-    rt = _Radix()
-    for ip_addr in addresses:
-        results = country_code_all(ip_addr, filter_nones=True)
-        if oracle in results: continue # authoritative source
-        # if only <2 dbs could geolocate => potential anomaly
-        if len(results) == 1: rt.add(ip_addr)
-        # not one unique answer for all dbs => potential anomaly
-        elif len(set(results.values())) != 1: rt.add(ip_addr)
-    return rt
-
 def quickload():
     '''
     Loads all known geolocation services using their default filenames from the given working directory.
+    Looks in the data folder.
     :param data_dir: directory containing files
     '''
     data_dir = "data"
-    load_db("geoip", data_dir + "/GeoIP.dat")
-    load_db("ip2location", data_dir + "/IP-COUNTRY.bin")
-    load_db("known_networks", data_dir + "/known-networks.json")
-    load_db("dbip", data_dir + "/dbip-country-2015-07.csv")
-    load_db("ipligence", data_dir + "/ipligence-lite.csv")
+    load("geoip", data_dir + "/GeoIP.dat")
+    load("ip2location", data_dir + "/IP-COUNTRY.bin")
+    load("dbip", data_dir + "/dbip-country-2015-07.csv")
+    load("ipligence", data_dir + "/ipligence-lite.csv")
 
-def load_db(db_name, db_fpath):
+def load(db_name, db_fpath):
     '''
     Load a geolocation service.
     :param db_name: name of the geolocation service.
@@ -139,15 +51,6 @@ def load_db(db_name, db_fpath):
     # load appropriate database
     if db_name == "geoip": _dbs[db_name] = GeoIP.open(db_fpath, GeoIP.GEOIP_STANDARD)
     elif db_name == "ip2location": _dbs[db_name] = IP2Location.IP2Location(db_fpath)
-    elif db_name == "known_networks":
-        _dbs['known_networks'] = _Radix()
-        with open(db_fpath, 'rb') as known_net_file:
-            known_networks = _json.load(known_net_file)
-        for p in known_networks:
-            # Only keep prefices whose country we know
-            if 'country' in p:
-                n = _dbs['known_networks'].add(p['net'])
-                n.data['cc'] = p['country']
     elif db_name == "dbip": _dbs[db_name] = _loadDBIP(db_fpath)
     elif db_name == "ipligence": _dbs[db_name] = IPligence(db_fpath)
     else: raise ValueError("error loading db {}".format(db_name))
@@ -155,6 +58,11 @@ def load_db(db_name, db_fpath):
     # update table of supported databases
     global available_dbs
     available_dbs = [k for k,v in _dbs.items() if v is not None]
+
+def clear():
+    global _dbs, available_dbs
+    _dbs = { k : None for k in _dbs }
+    available_dbs = []
 
 def query(to_query, ip_addr, db_name):
     '''
@@ -166,7 +74,7 @@ def query(to_query, ip_addr, db_name):
     :return: str result of the query.
     '''
     if to_query not in supported_queries: raise ValueError("Query not supported for {}".format(to_query))
-    if to_query == "country_code": return country_code(ip_addr, db_name)
+    if to_query == "country_code": return cc(ip_addr, db_name)
     else: raise ValueError("Could not perform {} query in {} database".format(to_query, db_name))
 
 def query_all(to_query, ip_addr, filter_nones=False):
@@ -180,13 +88,12 @@ def query_all(to_query, ip_addr, filter_nones=False):
     if not filter_nones: return results
     else: return { k : v for k,v in results.items() if v is not None }
 
-def country_code(ip_addr, db_name):
+def cc(ip_addr, db_name):
     '''
     Query the country code of the ip address from the specified database.
     :param ip_addr: ip address to query
     :param db_name: geolocation db to query from
     :return: country as given by the db. Raises ValueError if db has not yet been created.
-             Returns None if the db didn't know the answer.
     '''
 
     # validate query
@@ -202,10 +109,11 @@ def country_code(ip_addr, db_name):
         rt_node = _dbs[db_name].search_best(network=ip_addr, masklen=32)
         return None if rt_node is None else rt_node.data['cc']
     elif db_name == 'dbip': return _dbs[db_name][ip_addr]
-    elif db_name == 'ipligence': _dbs[db_name].country_code(ip_addr)
+    elif db_name == 'ipligence': _dbs[db_name].cc(ip_addr)
     else: raise ValueError("error querying {} db".format(db_name))
 
-def country_code_all(ip_addr, filter_nones=False):
+
+def cc_all(ip_addr, filter_nones=False):
     '''
     Query all databases for the country code of the ip address.
     :param ip_addr: ip address to look up
@@ -213,15 +121,9 @@ def country_code_all(ip_addr, filter_nones=False):
                 which map to None (these mean the database didn't have an entry for that ip)
     :return: { str : str }, a mapping of database -> result
     '''
-    results = { db : country_code(ip_addr, db) for db in available_dbs }
+    results = { db : cc(ip_addr, db) for db in available_dbs }
     if not filter_nones: return results
     else: return { k : v for k,v in results.items() if v is not None }
-
-def oracle(db_name):
-    if db_name not in _dbs: raise ValueError("Setting unknown db {} to oracle".format(db_name))
-    global oracle
-    oracle = db_name
-
 
 
 ## Geoloc private/helper functions.
@@ -299,7 +201,7 @@ class IPligence(object):
             # store the ending interval for last range
             me.mapping[end] = None
 
-    def country_code(me, ip_addr):
+    def cc(me, ip_addr):
         '''
         Look up country code of an address.
         :param ip_addr: ip address to look up.
